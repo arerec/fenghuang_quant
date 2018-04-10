9e9158f2854210024bd181aa336847bf73792b45
liangzhipeng <liangzhipeng@digbull.com>
2018-04-10 19:56:18 +0800

    初始文件

---
+++ config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+ORIGINAL_DATA_FILEPATH = r"C:\Users\lzp\Desktop\凤凰金融\round1"
+MAX_DAY = 488
---
+++ read_stock_tool.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__Created__ = '2018/4/10'
+
+import csv
+import numpy as np
+from config import ORIGINAL_DATA_FILEPATH, MAX_DAY
+
+class ReadStockTool(object):
+    def __init__(self, stock_code):
+        self.stock_code = stock_code
+
+
+    def read_one_stock(self):
+        stockdata_list = []
+        if isinstance(self.stock_code, int):
+            self.stock_code = str(self.stock_code)
+        for i in range(1, MAX_DAY + 1):
+            file_path = ORIGINAL_DATA_FILEPATH + "/" + str(i) + ".csv"
+            with open(file_path) as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if row[0] == "stock" + self.stock_code:
+                        stockdata_list.append(row[1])
+                        break
+        return stockdata_list
+
+    def draw_chart(self):
+        """
+        画图
+        :return:
+        """
+
+
+if __name__ == "__main__":
+    rst = ReadStockTool(102419)
+    data = rst.read_one_stock()
+    print(data)
